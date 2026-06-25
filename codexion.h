e10c3a849499d155d23bb7950291a488f82c1054
dioppolo <diego.ioppolo2006@gmail.com>
2026-06-25 10:08:26 +0200

    .h e main creati

---
+++ codexion.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dioppolo <dioppolo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/25 10:04:17 by dioppolo          #+#    #+#             */
+/*   Updated: 2026/06/25 10:07:38 by dioppolo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CODEXION_H
+# define CODEXION_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+
+
+#endif
